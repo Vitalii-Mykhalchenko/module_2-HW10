8662e04fa3712dbe80d365484946442aa9de7bb8
Vitalii-Mykhalchenko <swimming580@gmail.com>
2024-04-27 04:12:30 +0300

    add password reset

--- quote/users/forms.py
+++ quote/users/forms.py
@@ -1,12 +1,16 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class RegisterForm(UserCreationForm):
     username = forms.CharField(max_length=100,
                                required=True,
                                widget=forms.TextInput())
+    email = forms.CharField(max_length=100,
+                            required=True,
+                            widget=forms.TextInput())
 
     password1 = forms.CharField(max_length=50,
                                 required=True,
@@ -17,7 +21,7 @@ class RegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2']
 
 
 class LoginForm(AuthenticationForm):
@@ -25,3 +29,11 @@ class LoginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ['username', 'password']
+
+
+class ProfileForm(forms.ModelForm):
+    avatar = forms.ImageField(widget=forms.FileInput())
+
+    class Meta:
+        model = Profile
+        fields = ['avatar']
